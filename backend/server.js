ad7d2fe3f8efcd35b1c1860b8d7121d7e79dc2c9
ducluong-vn <luongnguyentrongduc@gmail.com>
2021-11-16 20:28:36 +0700

    check heroku init deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
+require("dotenv").config()
+
 const express = require("express")
 const app = express()
 const path = require("path")
+const mysql = require("mysql")
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+connection.connect((err) => {
+	if(err)
+		throw err;
+
+	console.log("Datbase connected.")
+})
 
 if (process.env.NODE_ENV === "production") {
-	const pathToFrontend = path.join(path.resolve(), "frontend")
+	const pathToFrontend = path.join(path.resolve(), "frontend", "build")
 
 	app.use(express.static(pathToFrontend))
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(pathToFrontend, "build", "index.html"))
+		res.sendFile(path.join(pathToFrontend, "index.html"))
 	})
 }
 
